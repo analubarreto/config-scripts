f1e6b774d4fbc5f4ab6b12d6fe7e7fa75e758608
Ana Luiza <45442311+analubarreto@users.noreply.github.com>
2020-05-04 20:49:35 -0300

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -11,14 +11,20 @@ echo '## Script start ##'
 echo '## Updating system ##'
 sudo apt update && sudo apt upgrade -y
 
-echo '## Install Discord and Spotify ##'
-sudo snap install discord spotify
+echo '## Install flatpak ##'
+sudo apt instal install flatpak
+
+echo '## Install Discord, Zoom and Odrive ##'
+sudo snap install discord zoom-client odrive-unofficial
 
 echo '## Install slack ##'
 sudo snap install slack --classic
 
 echo '## Install steam ##'
 sudo apt install steam
+
+echo '## Install Tidal ##'
+flatpak install nuvola eu.tiliado.NuvolaAppTidal 
 
 echo 'All done! Thanks for using this script! :)'
 ### FINISHED SCRIPT ###
