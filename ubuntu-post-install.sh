52effd8120bef188c35be967e4e417f57e8ae9a8
Ana Luiza <45442311+analubarreto@users.noreply.github.com>
2020-05-07 18:47:20 -0300

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -1,17 +1,13 @@
-## Ubuntu post install for devs
-#### VARIABLES
-####
-## Sets github variables
-## for later installation
-####
-
+## Ubuntu post install
 #### MAIN
 
 echo 'Are you a media creator (y/n)?'
 read media_creator
 
-if [media_creator -eq 'y'] then
+if [$media_creator -eq 'y']
+then
   echo 'Do you want to use Gravit Designer or Inkscape (gravit-designer/inkscape)?'
+  read design_app
 fi
 
 echo 'Do you want to install ODrive (y/n)'
@@ -19,15 +15,16 @@ read odrive
 
 echo '## Script start ##'
 echo '## Updating system ##'
-sudo apt update && sudo apt upgrade -y
+# sudo apt update && sudo apt upgrade -y
 
 echo '## Install flatpak and snap ##'
-sudo apt instal install flatpak snapd
+sudo apt install flatpak snapd -y
 
 echo '## Install Discord, Zoom, Odrive and Figma ##'
-sudo snap install discord zoom-client odrive-unofficial
+sudo snap install discord zoom-client
 
-if [odrive -eq 'y'] then
+if [$odrive -eq 'y']
+then
   echo '## Install ODrive ##'
   sudo snap install odrive-unofficial
 fi
@@ -38,12 +35,15 @@ sudo snap install slack --classic
 echo '## Install steam ##'
 sudo apt install steam -y
 
-if[media_creator -eq 'y'] then
+if[$media_creator -eq 'y'] then
   echo '## Install blender ##'
   sudo snap install blender --classic
 
-  echo '## Install figma obs-studio and "$design_app" ##'
+  echo '## Install figma and obs-studio ##'
   sudo snap install figma-linux obs-studio $design_app
+  
+  echo '## Install  Design App ##'
+  sudo snap install $design_app
 fi
 
 echo 'All done! Thanks for using this script! :)'
