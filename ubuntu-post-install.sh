9058048487676efe6a7378aa0e88a65f6a63a757
Ana Luiza <45442311+analubarreto@users.noreply.github.com>
2020-05-07 16:04:56 -0300

    Added optional apps

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -7,21 +7,44 @@
 
 #### MAIN
 
+echo 'Are you a media creator (y/n)?'
+read media_creator
+
+if [media_creator -eq 'y'] then
+  echo 'Do you want to use Gravit Designer or Inkscape (gravit-designer/inkscape)?'
+fi
+
+echo 'Do you want to install ODrive (y/n)'
+read odrive
+
 echo '## Script start ##'
 echo '## Updating system ##'
 sudo apt update && sudo apt upgrade -y
 
-echo '## Install flatpak ##'
-sudo apt instal install flatpak
+echo '## Install flatpak and snap ##'
+sudo apt instal install flatpak snapd
 
 echo '## Install Discord, Zoom, Odrive and Figma ##'
-sudo snap install discord zoom-client odrive-unofficial figma-linux
+sudo snap install discord zoom-client odrive-unofficial
+
+if [odrive -eq 'y'] then
+  echo '## Install ODrive ##'
+  sudo snap install odrive-unofficial
+fi
 
 echo '## Install slack ##'
 sudo snap install slack --classic
 
 echo '## Install steam ##'
 sudo apt install steam -y
+
+if[media_creator -eq 'y'] then
+  echo '## Install blender ##'
+  sudo snap install blender --classic
+
+  echo '## Install figma obs-studio and "$design_app" ##'
+  sudo snap install figma-linux obs-studio $design_app
+fi
 
 echo 'All done! Thanks for using this script! :)'
 ### FINISHED SCRIPT ###
