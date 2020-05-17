927ee9d3753541cd2f883f71dda45d4f0d303402
Ana Luiza <45442311+analubarreto@users.noreply.github.com>
2020-05-17 20:57:47 -0300

    Update flutter-script.sh

--- flutter-script.sh
+++ flutter-script.sh
@@ -6,12 +6,11 @@ echo 'Updating system and installing git'
 sudo apt update && sudo apt upgrade -y
 sudo apt install git
 
+echo 'Installing curl'
+sudo apt install curl
+
 echo 'Downloading flutter from github'
 git clone https://github.com/flutter/flutter.git -b stable
-
-echo 'Copying into development folder'
-mkdir development
-mv flutter $HOME/development
 
 echo 'Adding flutter tool to path'
 export PATH="$PATH:`pwd`/flutter/bin"
@@ -26,7 +25,7 @@ echo 'Download Android Studio using snap'
 sudo snap install android-studio --classic
 
 echo 'Adding flutter permanently to $PATH'
-export PATH="$PATH:$HOME/development/flutter/bin"
+export PATH="$PATH:$HOME/flutter/bin"
 which flutter
 
 echo 'Running flutter doctor'
